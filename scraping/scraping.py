74684123421726e00c97bceacc607ebeebe6df7c
Nathan W Johnson <Nathanjohnson482@gmail.com>
2024-03-06 23:47:57 -0600

    Added comment and updated

--- scraping/scraping.py
+++ scraping/scraping.py
@@ -1,36 +1,57 @@
-# Scraping for yelp using BeautifulSoup
+# Scraping for yelp using BeautifulSoup and requests
+
+# imports used
+import re
 import requests
 from bs4 import BeautifulSoup
-import numpy as np
-import pandas as pd
-import re
 
-counter = 0
+# Method to Scrap all of the reviews from a given yelp url.
+def get_yelp_reviews(url: str, stop_early: bool) -> list:
 
-string = 'https://www.yelp.com/biz/buffalo-joes-evanston'
-original_length = len(string)
-extra_string = '?start='
+    # If the user wants to stop early. Ask how many reviews they would like
+    if stop_early:
+        max_reviews = int(input('How many reviews would you like (Rounded up by ten)? '))
 
-r = requests.get(string)
-regex = re.compile('.*comment.*')
-reviews = []
-still_pages = True
-while(still_pages):
-    if (counter == 30):
-        break
-    try:
-        soup = BeautifulSoup(r.text, 'html.parser')
+    # Get the length of the url which we would use for splicing later.
+    original_url_length = len(url)
+    # Initialize list with the reviews for the return variable.
+    reviews = []
+    # Initialize the counter, and still_pages for the while loop 
+    counter = 0
+    still_pages = True
+    # Initialize the regex which will scan 
+    # the soup file for all of the reviews.
+    regex = re.compile('.*comment.*')
+    # Keep looping until theyre no more pages to scrap or, 
+    # the user specified max amount of reviews was reached. 
+    while still_pages:
+        # If the user wants to stop early and we've reached the maximum reviews.
+        if (stop_early and counter > max_reviews):
+            # Break the while loop
+            still_pages = False
+        # Make the soup to pull everything off that webpage.
+        soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+        # Pull all of the reviews using regex.
         results = soup.find_all('p', {'class':regex})
+        # If there are no reviews inside of results we know its out of pages.
+        if not results:
+            still_pages = False
+        # Take text values from each review and put it into a list.
         new_reviews = [result.text for result in results]
+        # Add the new reviews into the reviews array
         reviews.extend(new_reviews)
+        # Increment the counter by 10 which will "change the page" for yelp.
         counter += 10
-        string = string[:original_length]
-        string = f"{string}{extra_string}{counter}"
-        print(string)
-        r = requests.get(string)
-    except:
-        print("out of pages, ending loop")
-        still_pages = False
+        print(f'Page scraped. URL: {url}')
+        # Update the url.
+        url = f"{url[:original_url_length]}{'?start='}{counter}"
+    # Return all of the reviews
+    return reviews
         
-print(len(reviews))
-print(reviews[0])
+
+# Sample use
+all_reviews = get_yelp_reviews('https://www.yelp.com/biz/buffalo-joes-evanston', True)
+print(len(all_reviews))
+print(f"First review: {all_reviews[0]}")
+print('')
+print(f"Last  review: {all_reviews[len(all_reviews)-1]}")
